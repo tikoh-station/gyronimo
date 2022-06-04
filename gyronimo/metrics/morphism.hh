2ef4184294839c999204b6f51d4909888d8c1c9b
tikoh-station <mannyassuncao@hotmail.com>
2022-06-04 20:08:33 +0100

    Add member functions to morphism class for more compatibility

--- gyronimo/metrics/morphism.hh
+++ gyronimo/metrics/morphism.hh
@@ -1,3 +1,22 @@
+// ::gyronimo:: - gyromotion for the people, by the people -
+// An object-oriented library for gyromotion applications in plasma physics.
+// Copyright (C) 2022 Paulo Rodrigues.
+
+// ::gyronimo:: is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// ::gyronimo:: is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ::gyronimo::.  If not, see <https://www.gnu.org/licenses/>.
+
+// @morphism.hh, this file is part of ::gyronimo::
+
 #ifndef GYRONIMO_MORPHISM
 #define GYRONIMO_MORPHISM
 
@@ -24,12 +43,14 @@ public:
 
 	//! Returns the morphism's derivatives, correspondent to the covariant basis vectors in point `q`.
 	virtual dIR3 del(const IR3 &q) const = 0;
+	virtual dIR3 tan_basis(const IR3& q) const {return del(q);};
 
 	//! Returns the jacobian of the transformation in point `q`.
 	virtual double jacobian(const IR3 &q) const;
 
 	//! Returns the morphism's inverse derivatives, correspondent to the contravariant basis vectors in point `q`.
 	virtual dIR3 del_inverse(const IR3 &q) const;
+	virtual dIR3 dual_basis(const IR3& q) const {return del_inverse(q);};
 
 	//! Returns the covariant components of cartesian `A` in position `q`.
 	virtual IR3 to_covariant(const IR3 &q, const IR3 &A) const;
