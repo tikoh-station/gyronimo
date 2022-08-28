edbfdf2920429ae2fd89a985dc0584b6b813ee0c
tikoh-station <mannyassuncao@hotmail.com>
2022-08-28 15:48:22 +0100

    Add boris_advanced stepper class

---
+++ gyronimo/dynamics/boris_advanced.cc
@@ -0,0 +1,194 @@
+#include <gyronimo/dynamics/boris_advanced.hh>
+
+namespace gyronimo {
+
+// Performs a time step `dt` update to the state `in` and returns the result.
+boris_advanced::state boris_advanced::do_step(const boris_advanced::state &in, const double t, const double dt) const {
+
+	// extract position and velocity from state
+	IR3 qm1 = {in[0], in[1], in[2]};
+	IR3 umh = {in[3], in[4], in[5]};
+	IR3 qmh = {in[6], in[7], in[8]};
+
+	if(field_morph_) {
+
+		IR3 qk = qm1 + umh * dt;
+		dIR3 ek  = field_morph_->del(qk);
+		dIR3 eek = inverse(ek);
+
+		IR3 vmh = field_morph_->from_contravariant(qmh, umh);
+
+		// calculate fields
+		IR3 Ek = {0, 0, 0};
+		IR3 Bk = {0, 0, 0};
+		if(electric_) {
+			Ek = electric_->contravariant(qk, (t+dt) * iEfield_time_factor_);
+			Ek = contraction<second>(ek, Ek);
+		}
+		if(magnetic_) {
+			Bk = magnetic_->contravariant(qk, (t+dt) * iBfield_time_factor_);
+			Bk = contraction<second>(ek, Bk);
+		}
+
+		// perform cartesian step
+		IR3 vph = cartesian_boris(vmh, Oref_, Ek, Bk, dt);
+		IR3 qph = qk + (0.25 * dt) * contraction<second>(eek, vph + vmh);
+		dIR3 eeph = field_morph_->del_inverse(qph);
+		IR3 uph = contraction<second>(eeph, vph);
+
+		return { qk[IR3::u],  qk[IR3::v],  qk[IR3::w],
+				uph[IR3::u], uph[IR3::v], uph[IR3::w],
+				qph[IR3::u], qph[IR3::v], qph[IR3::w]};
+
+	} else {
+
+		IR3 xk = qm1 + umh * dt;
+
+		// calculate fields
+		IR3 Ek = {0, 0, 0};
+		IR3 Bk = {0, 0, 0};
+		if(electric_) Ek = electric_->contravariant(xk, (t+dt) * iEfield_time_factor_);
+		if(magnetic_) Bk = magnetic_->contravariant(xk, (t+dt) * iBfield_time_factor_);
+
+		// perform cartesian step
+		IR3 vph = cartesian_boris(umh, Oref_, Ek, Bk, dt);
+
+		IR3 xph = xk + (0.5 * dt) * vph;
+
+		return { xk[IR3::u],  xk[IR3::v],  xk[IR3::w],
+				vph[IR3::u], vph[IR3::v], vph[IR3::w],
+				xph[IR3::u], xph[IR3::v], xph[IR3::w]};
+
+	}
+
+}
+
+// Returns the vector position of the state normalized to `Lref`.
+IR3 boris_advanced::get_position(const boris_advanced::state& s) const {
+	return {s[0], s[1], s[2]};
+}
+
+// Returns the vector velocity of the state normalized to `Vref`.
+IR3 boris_advanced::get_velocity(const boris_advanced::state& s) const {
+	return {s[3], s[4], s[5]};
+}
+
+// Returns the kinetic energy of the state, normalized to `Uref`.
+double boris_advanced::get_kinetic_energy(const boris_advanced::state& s) const {
+	if(field_morph_) {
+
+		IR3 qmh = {s[6], s[7], s[8]};
+		IR3 umh = {s[3], s[4], s[5]};
+		dIR3 emh = field_morph_->del(qmh);
+		IR3 vmh = contraction<second>(emh, umh);
+
+		return vmh[IR3::u] * vmh[IR3::u] + vmh[IR3::v] * vmh[IR3::v] + vmh[IR3::w] * vmh[IR3::w];
+
+	} else return s[3]*s[3] + s[4]*s[4] + s[5]*s[5];
+}
+
+// Returns the parallel energy of the state, normalized to `Uref`.
+double boris_advanced::get_parallel_energy(const boris_advanced::state& s, double &time) const {
+	IR3 q = {s[6], s[7], s[8]};
+	IR3 u = {s[3], s[4], s[5]};
+	if(magnetic_) {
+		IR3 b = magnetic_->contravariant_versor(q, time * iBfield_time_factor_);
+		if(field_morph_) {
+			IR3 v = field_morph_->from_contravariant(q, u);
+			IR3 u_cov = field_morph_->to_covariant(q, v);
+			double vpp = inner_product(u_cov, b);
+			return vpp * vpp;
+		} else {
+			double vpp = inner_product(u, b);
+			return vpp * vpp;
+		}
+	} else {
+		error(__func__, __FILE__, __LINE__, "null magnetic field.", 1);
+		return 0;
+	}
+}
+
+// Returns the perpendicular energy of the state, normalized to `Uref`.
+double boris_advanced::get_perpendicular_energy(const boris_advanced::state& s, double &time) const {
+	IR3 q = {s[6], s[7], s[8]};
+	IR3 u = {s[3], s[4], s[5]};
+	if(magnetic_) {
+		IR3 b = magnetic_->contravariant_versor(q, time * iBfield_time_factor_);
+		if(field_morph_) {
+			IR3 v = field_morph_->from_contravariant(q, u);
+			IR3 b_cartesian = field_morph_->from_contravariant(q, b);
+			IR3 vperp = cartesian_cross_product(v, b_cartesian);
+			return inner_product(vperp, vperp);
+		} else {
+			IR3 vperp = cartesian_cross_product(u, b);
+			return inner_product(vperp, vperp);
+		}
+	} else {
+		error(__func__, __FILE__, __LINE__, "null magnetic field.", 1);
+		return 0;
+	}
+}
+
+// Returns the `boris_advanced::state` from a normalized point in phase-space.
+boris_advanced::state boris_advanced::generate_state(const IR3 &pos, const IR3 &vel, const IR3 &pph) const {
+	return {pos[IR3::u], pos[IR3::v], pos[IR3::w],
+			vel[IR3::u], vel[IR3::v], vel[IR3::w],
+			pph[IR3::u], pph[IR3::v], pph[IR3::w]};
+}
+
+// Creates the first `boris_advanced::state` from a normalized point in cartesian phase-space.
+boris_advanced::state boris_advanced::generate_initial_state(
+		const IR3 &cartesian_position, const IR3 &cartesian_velocity, const double &tinit, const double &dt) const {
+
+	electromagnetic_system em(1, 1, qom_, electric_, magnetic_, field_morph_);
+	odeint_adapter<electromagnetic_system> sys(&em);
+	boost::numeric::odeint::runge_kutta4<electromagnetic_system::state> rk4;
+
+	IR3 qk = cartesian_position;
+	IR3 uk = cartesian_velocity;
+	if(field_morph_) {
+		qk = field_morph_->inverse(cartesian_position);
+		uk = field_morph_->to_contravariant(qk, cartesian_velocity);
+	}
+	electromagnetic_system::state in = em.generate_state(qk, uk);
+	electromagnetic_system::state out;
+
+	rk4.do_step(sys, in, tinit, out, 0.5*dt);
+	IR3 qph = em.get_position(out);
+	IR3 uph = em.get_velocity(out);
+
+	return generate_state(qk, uph, qph);
+}
+
+// Performs a boris step in cartesian coordinates.
+IR3 boris_advanced::cartesian_boris(const IR3 &v_old, const double &Oref, const IR3 &Ek, const IR3 &Bk, const double &dt) const {
+
+	// step 1
+	IR3 half_E_impulse = {0, 0, 0};
+	IR3 v_new = v_old;
+	if(electric_) {
+
+		half_E_impulse = (0.5 * Oref * dt) * Ek;
+		v_new += half_E_impulse;
+	}
+
+	// step 2
+	if(magnetic_) {
+
+		double B = std::sqrt(inner_product(Bk, Bk));
+		IR3 b = B!=0 ? (1/B) * Bk : Bk;
+
+		double t = std::tan((0.5 * Oref * dt) * B);
+		double s = (2 * t) / (1 + t*t);
+
+		IR3 v_prime = v_new + t * cartesian_cross_product(v_new, b);
+		v_new += s * cartesian_cross_product(v_prime, b);
+	}
+
+	// step 3
+	if(electric_) v_new += half_E_impulse;
+
+	return v_new;
+}
+
+} // end namespace gyronimo
---
+++ gyronimo/dynamics/boris_advanced.hh
@@ -0,0 +1,100 @@
+
+#ifndef GYRONIMO_BORIS_ADVANCED
+#define GYRONIMO_BORIS_ADVANCED
+
+#include <cmath>
+
+#include <gyronimo/core/error.hh>
+#include <gyronimo/core/codata.hh>
+#include <gyronimo/fields/IR3field.hh>
+#include <gyronimo/metrics/morphism.hh>
+#include <gyronimo/dynamics/electromagnetic_system.hh>
+#include <gyronimo/dynamics/odeint_adapter.hh>
+
+#include <boost/numeric/odeint.hpp>
+
+namespace gyronimo {
+
+//! Gyronimo implementation for classical boris stepper class.
+class boris_advanced {
+
+public:
+
+	//! State variable of the stepper.
+	typedef std::array<double,9> state;
+
+	//! Class Constructor.
+	boris_advanced(const double Lref, const double Vref, const double qom, const IR3field *E, const IR3field *B, const morphism *morph = NULL)
+			: Lref_(Lref), Vref_(Vref), Tref_(Lref/Vref), qom_(qom),
+			Oref_(	B ? qom * gyronimo::codata::e / gyronimo::codata::m_proton * B->m_factor() : 
+					E ? qom * gyronimo::codata::e / gyronimo::codata::m_proton * E->m_factor() : 0), 
+			electric_(E), magnetic_(B),
+			iBfield_time_factor_(B ? Tref_ / B->t_factor() : 0.0),
+			iEfield_time_factor_(E ? Tref_ / E->t_factor() : 0.0), 
+			field_morph_(morph) {
+	
+		//@todo test if fields are consistent (normalization and cartesian metric)
+	};
+
+	//! Class Destructor.
+	~boris_advanced() {};
+
+	//! Returns the order of the method.
+	unsigned short order() const {return 2;};
+
+	//! Performs a time step `dt` update to the state `in` and returns the result.
+	state do_step(const state &in, const double t, const double dt) const;
+
+	//! Returns the vector position of the state normalized to `Lref`.
+	IR3 get_position(const state& s) const;
+	//! Returns the vector velocity of the state normalized to `Vref`.
+	IR3 get_velocity(const state& s) const;
+	//! Returns the kinetic energy of the state, normalized to `Uref`.
+	double get_kinetic_energy(const state& s) const;
+	double get_parallel_energy(const state& s, double &time) const;
+	double get_perpendicular_energy(const state& s, double &time) const;
+
+	//! Returns the `curvilinear_boris::state` from a normalized point in phase-space.
+	state generate_state(const IR3 &position, const IR3 &velocity, const IR3 &velocity_position) const;
+
+	// Creates the first `boris_advanced::state` from a normalized point in cartesian phase-space.
+	state generate_initial_state(const IR3 &cartesian_position, 
+		const IR3 &cartesian_velocity, const double &tinit, const double &dt) const;
+
+
+	//! Returns the reference gyration frequency scale `Oref`.
+	double Oref() const {return Oref_;};
+	//! Returns the pointer to the electric field.
+	const IR3field* electric() const {return electric_;};
+	//! Returns the pointer to the magnetic field.
+	const IR3field* magnetic() const {return magnetic_;};
+	//! Returns the pointer to the morphism.
+	const morphism* field_morph() const {return field_morph_;};
+	//! Returns the pointer to the morphism.
+	const morphism* inner_morph() const {return field_morph_;};
+
+	//! Returns the reference length scale `Lref`.
+	double Lref() const {return Lref_;};
+	//! Returns the reference time scale `Tref`.
+	double Tref() const {return Tref_;};
+	//! Returns the reference velocity scale `Vref`.
+	double Vref() const {return Vref_;};
+
+private:
+
+	const double Lref_, Vref_, Tref_;
+	const double qom_, Oref_;
+	const IR3field *electric_;
+	const IR3field *magnetic_;
+	const double iBfield_time_factor_;
+	const double iEfield_time_factor_;
+	const morphism *field_morph_;
+
+	//! Performs a boris step in cartesian coordinates.
+	IR3 cartesian_boris(const IR3 &v_old, const double &Oref, const IR3 &Ek, const IR3 &Bk, const double &dt) const;
+
+}; // end class boris_advanced
+
+} // end namespace gyronimo
+
+#endif // GYRONIMO_BORIS_ADVANCED
